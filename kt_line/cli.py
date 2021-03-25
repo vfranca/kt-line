2fe803ffd68bcce0edb9d756f643b4725ac32423
Valmir Franca <vfranca3@gmail.com>
2021-03-25 03:32:13 -0300

    Adicionada opcao --version

--- kt_line/cli.py
+++ kt_line/cli.py
@@ -1,13 +1,26 @@
+"""
+Scripts do console
+"""
 from kt_line import kt_line
+from kt_line import __version__
 import click
 
 
 @click.command()
-@click.argument("a")
-@click.argument("b")
-def cli(a, b):
-    if a and b:
-        click.echo(kt_line.line(float(a), float(b)))
+@click.argument("preco-a", type=float, required=False)
+@click.argument("preco-b", type=float, required=False)
+@click.option("--version", is_flag=True, help="Exibe a versao")
+def cli(preco_a, preco_b, version):
+    """Calcula proximo preco da linha de canal."""
+    if version:
+        click.echo("kt-line %s" % __version__)
+        return 0
+    if not preco_a:
+        click.echo("Usage: l [OPTIONS] [PRECO-A] [PRECO-B]")
+        return 0
+    preco_c = kt_line.line(preco_a, preco_b)
+    click.echo(preco_c)
+    return 0
 
 
 if __name__ == "__main__":
